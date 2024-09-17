3167272b56567479f659ee3ad0ce73656a8f533d
Mohammad Moradi Esfahaniasl <au726845@uni.au.dk>
2024-09-17 17:12:57 +0200

    Make the probanbility function more precise

--- src/utilities/general_functions.rs
+++ src/utilities/general_functions.rs
@@ -6,47 +6,91 @@ pub mod energy_functions{
     {
         let mut generator = rand::thread_rng();
         let decision = match hour {
-            0..=5 => {
+            0..=2 => {
                 match item.get_appliance_name().as_str() {
-                    "Heat Pump" => generator.gen_bool(0.7),
+                    "Heat Pump" => generator.gen_bool(0.6),
                     "Refrigerator" => true,
-                    "TV" => generator.gen_bool(0.1),
+                    "TV" => generator.gen_bool(0.3),
                     "Washing Machine" => generator.gen_bool(0.1),
-                    "Dishwashser" => generator.gen_bool(0.15),
+                    "Dishwashser" => generator.gen_bool(0.1),
                     "CookingStove" => generator.gen_bool(0.1),
                     _ => false,
                 }
             },
-            6..=12 => {
+            3..=5 => {
                 match item.get_appliance_name().as_str() {
-                    "Heat Pump" => generator.gen_bool(0.7),
+                    "Heat Pump" => generator.gen_bool(0.6),
                     "Refrigerator" => true,
-                    "TV" => generator.gen_bool(0.8),
-                    "Washing Machine" => generator.gen_bool(0.3),
-                    "Dishwashser" => generator.gen_bool(0.6),
-                    "CookingStove" => generator.gen_bool(0.8),
+                    "TV" => generator.gen_bool(0.1),
+                    "Washing Machine" => generator.gen_bool(0.1),
+                    "Dishwashser" => generator.gen_bool(0.1),
+                    "CookingStove" => generator.gen_bool(0.1),
                     _ => false,
                 }
             },
-            13..=18 => {
+            6..=8 => {
                 match item.get_appliance_name().as_str() {
                     "Heat Pump" => generator.gen_bool(0.4),
                     "Refrigerator" => true,
-                    "TV" => generator.gen_bool(0.8),
-                    "Washing Machine" => generator.gen_bool(0.3),
+                    "TV" => generator.gen_bool(0.3),
+                    "Washing Machine" => generator.gen_bool(0.1),
                     "Dishwashser" => generator.gen_bool(0.2),
                     "CookingStove" => generator.gen_bool(0.4),
                     _ => false,
                 }
             },
-            19..=23 => {
+            9..=11 => {
                 match item.get_appliance_name().as_str() {
-                    "Heat Pump" => generator.gen_bool(0.7),
+                    "Heat Pump" => generator.gen_bool(0.3),
+                    "Refrigerator" => true,
+                    "TV" => generator.gen_bool(0.6),
+                    "Washing Machine" => generator.gen_bool(0.2),
+                    "Dishwashser" => generator.gen_bool(0.4),
+                    "CookingStove" => generator.gen_bool(0.8),
+                    _ => false,
+                }
+            },
+            12..=14 => {
+                match item.get_appliance_name().as_str() {
+                    "Heat Pump" => generator.gen_bool(0.2),
                     "Refrigerator" => true,
                     "TV" => generator.gen_bool(0.4),
-                    "Washing Machine" => generator.gen_bool(0.7),
-                    "Dishwashser" => generator.gen_bool(0.7),
-                    "CookingStove" => generator.gen_bool(0.8),
+                    "Washing Machine" => generator.gen_bool(0.4),
+                    "Dishwashser" => generator.gen_bool(0.3),
+                    "CookingStove" => generator.gen_bool(0.5),
+                    _ => false,
+                }
+            },
+            15..=17 => {
+                match item.get_appliance_name().as_str() {
+                    "Heat Pump" => generator.gen_bool(0.3),
+                    "Refrigerator" => true,
+                    "TV" => generator.gen_bool(0.3),
+                    "Washing Machine" => generator.gen_bool(0.3),
+                    "Dishwashser" => generator.gen_bool(0.1),
+                    "CookingStove" => generator.gen_bool(0.3),
+                    _ => false,
+                }
+            },
+            18..=20 => {
+                match item.get_appliance_name().as_str() {
+                    "Heat Pump" => generator.gen_bool(0.4),
+                    "Refrigerator" => true,
+                    "TV" => generator.gen_bool(0.85),
+                    "Washing Machine" => generator.gen_bool(0.2),
+                    "Dishwashser" => generator.gen_bool(0.2),
+                    "CookingStove" => generator.gen_bool(0.7),
+                    _ => false,
+                }
+            },
+            21..=23 => {
+                match item.get_appliance_name().as_str() {
+                    "Heat Pump" => generator.gen_bool(0.5),
+                    "Refrigerator" => true,
+                    "TV" => generator.gen_bool(0.7),
+                    "Washing Machine" => generator.gen_bool(0.5),
+                    "Dishwashser" => generator.gen_bool(0.6),
+                    "CookingStove" => generator.gen_bool(0.3),
                     _ => false,
                 }
             },
